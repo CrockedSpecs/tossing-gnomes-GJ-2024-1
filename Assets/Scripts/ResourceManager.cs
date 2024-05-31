17bd2bfee36f74630cea436550b1c19d151f196f
CrookedSpecs <saparadaa@unal.edu.co>
2024-05-31 18:16:38 -0500

    sistema de creacion de recursos

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int WaterResources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaterResources = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
