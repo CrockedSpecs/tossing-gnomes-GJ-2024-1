27b0ca1ff2649f38fe8acfc137c1bf1ac79c2721
Daniel Felipe Agudelo Parra <d4niel.f.4.p4rr4@gmail.com>
2024-06-01 16:01:36 -0500

    adelanto

--- Assets/Scripts/EnemiesLogic/SpawnPoint/SpawnPoint.cs
+++ Assets/Scripts/EnemiesLogic/SpawnPoint/SpawnPoint.cs
@@ -2,25 +2,29 @@ using AYellowpaper.SerializedCollections;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using static UnityEditor.Progress;
 
 public class SpawnPoint : MonoBehaviour
 {
     [SerializeField]
+    LayerMask layer;
+
+    [SerializeField]
     SerializedDictionary<int, List<SpawnPointNode>> HordesManagement = new SerializedDictionary<int, List<SpawnPointNode>>();
     [SerializeField] float spawnInterval = 2.0f;
+
     [SerializeField] float spawnIntervalBetweenHordes = 2.0f;
-    [SerializeField] EnemyAbstractClass enemyAbstractClass;
 
-    EnemyAbstractClass enemyToSpawn;
+    [SerializeField] GameObject enemyAbstractClass;
 
-    bool toSpawn;
+    [SerializeField] GameObject TargetToDestroy;
 
-    public void CreateEnemy(EnemiesEnum enemiesEnum)
+    private void Awake()
     {
-        Type enemyType = Type.GetType($"Assets.Scripts.EnemiesLogic.EnemyTypes.{enemiesEnum}");
-        enemyToSpawn  = (EnemyAbstractClass) Activator.CreateInstance(enemyType);
-        //To spawn bad AI
+
     }
 
     void Start()
@@ -28,16 +32,128 @@ public class SpawnPoint : MonoBehaviour
         StartCoroutine(SpawnEnemy());
     }
 
+    private void FixedUpdate()
+    {
+        if (TargetToDestroy == null || !TargetToDestroy
+            .activeInHierarchy)
+        {
+            Vector3 direction = 
+                Vector3.zero - this.transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(this.transform.position, direction, out hit, direction.magnitude, layer))
+            {
+                // Si el rayo golpea un objeto en la capa especificada, guarda el GameObject en la variable
+                TargetToDestroy = hit.collider.gameObject;
+            }
+        }
+    }
+
     IEnumerator SpawnEnemy()
     {
         for (int i = 0; i < HordesManagement.Count; i++)
         {
-            for (int j = 0; j < HordesManagement[i].Count; j++)
+            foreach (var item in HordesManagement[i])
             {
-                Instantiate(enemyAbstractClass
-                    , transform.position
-                    , Quaternion.identity);
-                yield return new WaitForSeconds(spawnInterval);
+                for(int j = 0; j < item.NumberOfEnemies; j++)
+                {
+                    enemyAbstractClass = Resources
+                        .Load<GameObject>(item.enemiesEnum
+                        .ToString());
+
+                    /*Si el diccionario con objetos en uso
+                    ya contiene el tipo entonces*/
+                    Debug.Log(item.enemiesEnum.ToString());
+                    if (EnemyObjectPooling
+                        .sharedInstanceEnemyObjectPooling
+                        .objectPoolingInUse
+                        .ContainsKey
+                        (item.enemiesEnum.ToString()))
+                    {
+                        /*Si hay elementos desactivados disponibles activara uno y lo pondra en el spawnpoint*/
+                        if (EnemyObjectPooling
+                        .sharedInstanceEnemyObjectPooling
+                        .objectPooling[item.enemiesEnum
+                        .ToString()].Count != 0)
+                        {
+                            //enemyAbstractClass =
+                            //    EnemyObjectPooling
+                            //.sharedInstanceEnemyObjectPooling
+                            //.objectPooling[item.enemiesEnum
+                            //.ToString()][0];
+                            //
+
+                            EnemyObjectPooling
+                            .sharedInstanceEnemyObjectPooling
+                            .objectPooling[item.enemiesEnum
+                            .ToString()][0].SetActive(true);
+
+                            EnemyObjectPooling
+                            .sharedInstanceEnemyObjectPooling
+                            .objectPooling[item.enemiesEnum
+                            .ToString()][0].transform
+                                .position =
+                                new Vector3(
+                                this.transform.position.x
+                                , this.transform.position.y
+                                , this.transform.position.z);
+                        }
+                        //si no hay elementos no activos disponibles entonces creara uno nuevo
+                        else
+                        {
+                            enemyAbstractClass
+                                .GetComponent
+                                <EnemyAbstractClass>()
+                                .EnemyType = item.enemiesEnum;
+                            EnemyObjectPooling
+                            .sharedInstanceEnemyObjectPooling
+                            .objectPoolingInUse[item.enemiesEnum
+                            .ToString()].Add(
+                            Instantiate
+                                (enemyAbstractClass
+                                , transform.position
+                                , Quaternion.identity)
+                            );
+                        }
+                    }
+                    /*Si el diccionario con objetos en uso
+                    no contiene el tipo entonces creara un 
+                    nuevo elemento en el diccionario con el 
+                    tipo y una nueva lista, tanto el 
+                    diccionario en uso como en el disponible 
+                    */
+                    else
+                    {
+                        //añade primero el tipo al enemigo
+                        enemyAbstractClass
+                                .GetComponent
+                                <EnemyAbstractClass>()
+                                .EnemyType = item.enemiesEnum;
+                        EnemyObjectPooling
+                        .sharedInstanceEnemyObjectPooling
+                        .objectPooling.Add
+                        (item.enemiesEnum.ToString()
+                        , new List<GameObject>());
+
+                        EnemyObjectPooling
+                        .sharedInstanceEnemyObjectPooling
+                        .objectPoolingInUse.Add
+                        (
+                            item.enemiesEnum.ToString()
+                            , new List<GameObject>
+                            {
+                                Instantiate
+                                    (enemyAbstractClass
+                                    , transform.position
+                                    , Quaternion.identity)
+                            }
+                        );
+                    }
+
+                    yield return new 
+                        WaitForSeconds(spawnInterval);
+                }
+                yield return new
+                    WaitForSeconds(spawnInterval);
             }
             yield return new WaitForSeconds(spawnIntervalBetweenHordes);
         }
